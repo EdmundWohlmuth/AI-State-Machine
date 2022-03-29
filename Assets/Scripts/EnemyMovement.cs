0c544a7a560f658a6baf058f061c5a6827100999
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-03-29 11:32:09 -0300

    Initial commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.transform.position);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AI-State-Machine
+ 
